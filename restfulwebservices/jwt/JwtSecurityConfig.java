28c988ef210aba59a92fb8be42f3abe0bb7cdc6c
Daarshik Dwivedi <you@example.com>
2023-08-29 02:39:37 +0530

    Connected to mysql database

--- restfulwebservices/jwt/JwtSecurityConfig.java
+++ restfulwebservices/jwt/JwtSecurityConfig.java
@@ -50,7 +50,7 @@ public class JwtSecurityConfig {
         return httpSecurity
                 .authorizeHttpRequests(auth -> auth
                     .antMatchers("/authenticate").permitAll()
-                    .requestMatchers(PathRequest.toH2Console()).permitAll() // h2-console is a servlet and NOT recommended for a production
+//                    .requestMatchers(PathRequest.toH2Console()).permitAll() // h2-console is a servlet and NOT recommended for a production
                     .antMatchers(HttpMethod.OPTIONS,"/**")
                     .permitAll()
                     .anyRequest()
